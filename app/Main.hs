b51acbcfeac10c43f8ff62441118c437a6117c6b
aryzach <zach_asmith@yahoo.com>
2022-11-25 02:59:13 +0000

    commit after long time not touching this project, but there were uncommitted changes on server

--- app/Main.hs
+++ app/Main.hs
@@ -3,76 +3,88 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Debug.Trace
-import           Network.Socket                
-import qualified Network.Socket.ByteString     as SBS                
-
+import           Network.Socket
+import qualified Network.Socket.ByteString      as SBS
+import           Control.Concurrent             ( forkFinally
+    )
+import           Control.Exception              ( bracket
+    , bracketOnError
+    , handle
+    , throwIO
+    )
 import           Control.Monad                  ( forever
-                                                , void
-                                                )
---import qualified Data.ByteArray                as BA
+    , void
+    )
+import qualified Data.ByteArray                as BA
 import qualified Data.ByteString               as BS
-import           Data.ByteString.Char8         as C8 hiding (putStrLn, unsnoc, snoc, map, filter)
 import           Data.Default
 import           System.Exit
 
---import qualified System.Socket                  as S
---import qualified System.Socket.Family.Inet6     as S
---import qualified System.Socket.Protocol.Default as S
---import qualified System.Socket.Type.Stream      as S
---import qualified System.Socket.Unsafe           as S
+import qualified System.Socket                  as S
+import qualified System.Socket.Family.Inet      as S
+import qualified System.Socket.Protocol.Default as S
+import qualified System.Socket.Type.Stream      as S
+import qualified System.Socket.Unsafe           as S
 
 import           Network.SSH
-import           Network.SSH.Server            as Server
-import           Network.SSH.Internal
+import qualified Network.SSH.Server            as Server
+
 import           Data.Text                     as T hiding (map, filter)
 import           Data.Text.Encoding            as TE
 
+import           Data.ByteString.Char8         as C8 hiding (putStrLn, unsnoc, snoc, map, filter)
+
+-- tui
+import Graphics.Vty
+import System.Posix.Types
+import System.Posix.Terminal
+import System.Posix.IO.ByteString
+import System.Environment (lookupEnv)
+
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
+-- tui
+
+
 main :: IO ()
 main = do
-    _ <- putStrLn "start"
-    keyPair <- newKeyPair
-    _ <- putStrLn "before runServer"
-    let hints = defaultHints { addrFlags = [AI_NUMERICHOST, AI_NUMERICSERV], addrSocketType = Stream }
-    addr:_ <- getAddrInfo (Just hints) (Just "127.0.0.1") (Just "5000")
-    sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
-    Network.Socket.bind sock (addrAddress addr)
-    listen sock 1
-    (s,_) <- accept sock
-    putStrLn "after accept"
-    serve config keyPair sock
-    putStrLn "after serve"
-    pure ()
-  where
-    config = def
-        { --Server.socketConfig             = def { Server.socketBindAddresses = pure (Address "*" 2023)}
-          transportConfig          = def {
-                onSend = \x -> putStrLn ("CLIENT: " ++ show x),
-                onReceive = \x -> putStrLn ("SERVER: " ++ show x)
-            }
-        , userAuthConfig           = def
-            { onAuthRequest        = \username _ _ -> putStrLn "auth req" >> pure (Just username)
-            }
-        , connectionConfig         = def
-            { onSessionRequest     = handleSessionRequest
-            --, onDirectTcpIpRequest = handleDirectTcpIpRequest
-            }
-        --, onConnect                = \_ -> do
-        --    pure (Just ())
-        }
-
-
-handleSessionRequest :: (Show state, Show user) => state -> user -> IO (Maybe SessionHandler)
-handleSessionRequest state user = pure $ Just $ SessionHandler $ mySessionHandler state user BS.empty
+  privateKey <- newKeyPair
+  Server.runServer config privateKey
+    where
+        config = def
+                { Server.socketConfig             = def { Server.socketBindAddresses = pure (Address "172.31.14.100" 2023)}
+                , Server.transportConfig          = def 
+                        { onSend = \x -> putStrLn ("CLIENT: " ++ show x)
+                        , onReceive = \x -> putStrLn ("SERVER: " ++ show x)
+                        }
+                , Server.userAuthConfig           = def
+                        { Server.onAuthRequest        = \_ username _ _ _ -> pure (Just username)
+                        }
+                , Server.connectionConfig         = def
+                        { Server.onSessionRequest     = handleSessionRequest
+                        }
+                , Server.onConnect                = \_ -> do pure (Just ())
+                }
+    
+handleSessionRequest :: (Show user) => state -> user -> IO (Maybe Server.SessionHandler)
+handleSessionRequest state user = pure $ Just $ Server.SessionHandler $ mySessionHandler state user BS.empty
   
-mySessionHandler :: (Show state, Show user, InputStream stdin, OutputStream stdout, OutputStream stderr) => state -> user -> BS.ByteString -> Environment -> Maybe TermInfo -> Maybe Command -> stdin -> stdout -> stderr -> IO ExitCode     
+mySessionHandler :: (Show user, InputStream stdin, OutputStream stdout, OutputStream stderr) => state -> user -> BS.ByteString -> Environment -> Maybe TermInfo -> Maybe Command -> stdin -> stdout -> stderr -> IO ExitCode     
 mySessionHandler state user previousCommandBytes a b c stdin stdout d = do
-    p <- receive stdin 1024
-    let currentCommandBytes = (BS.append previousCommandBytes p)
-    sendAll stdout p
-    if lastIsCarriageReturn $ TE.decodeUtf8 p
+    userInput <- receive stdin 1024
+
+    let currentCommandBytes = (BS.append previousCommandBytes userInput)
+        line0 = string (defAttr ` withForeColor ` green) "first line"
+        line1 = string (defAttr ` withBackColor ` blue) "second line"
+        img = line0 <-> line1
+        pic = picForImage img
+        picAsBytes = C8.pack $ TL.unpack pic
+
+    sendAll stdout userInput
+    if lastIsCarriageReturn $ TE.decodeUtf8 userInput
       then do
         sendAll stdout $ C8.pack . createResponseFromCommand . C8.unpack $ currentCommandBytes
+        sendAll stdout picAsBytes
         mySessionHandler state user BS.empty a b c stdin stdout d 
       else do
         mySessionHandler state user currentCommandBytes a b c stdin stdout d 
@@ -111,31 +123,38 @@ isUserCommand _    = Nothing
 stripAll :: String -> String
 stripAll s = filter (\char -> char /= '\n' && char /= '\r') s
 
-instance DuplexStream Socket 
+{-
 
-instance InputStream Socket where
-  peek = SBS.recv
-  receive = SBS.recv
+     (r,w) <- openPseudoTerminal
+     cfg <- configWithPipe stdInput stdOutput
+     vty <- mkVty cfg
+     let line0 = string (defAttr ` withForeColor ` green) "first line"
+         line1 = string (defAttr ` withBackColor ` blue) "second line"
+         img = line0 <-> line1
+         pic = picForImage img
+     update vty pic
+     e <- nextEvent vty
+     shutdown vty
+     print ("Last event was: " ++ show e)
+     putStrLn $ show img
 
-instance OutputStream Socket where
-  send = SBS.send 
 
-{- TODO
- - ctrl-C to exit
- - strip whitespace before command
- - add '#' or whatever before user types
- - make game
- - figure out how to allow the play the game from the ssh session
- - make game with graphics similar to multi-facotr QR code from work
- -}
+configWithPipe :: Fd -> Fd -> IO Config
+configWithPipe readPipe writePipe = do
+    -- this should instead be from TermInfo on the client I think. This is a temp solution
+    mb <- lookupEnv "TERM"
+    print mb
+    case mb of
+      Nothing -> standardIOConfig
+      Just t -> do
+        return defaultConfig
+          { vmin               = Just 1
+          , mouseMode          = Just False
+          , bracketedPasteMode = Just False
+          , vtime              = Just 100
+          , inputFd            = Just readPipe
+          , outputFd           = Just writePipe
+          , termName           = Nothing
+          }
 
---createResponseContent :: T.Text -> T.Text
---createResponseContent text = T.pack $ 
-     
-
--- T.pack :: String -> Text
--- T.unpack :: Text -> String
--- C8.pack :: String -> ByteString
--- C8.unpack :: ByteString -> String
--- TE.encodeUtf8 :: Text -> ByteString
--- TE.decodeUtf8 :: ByteString -> Text
+-}
